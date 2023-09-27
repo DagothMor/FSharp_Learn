33ade940fd4f8131315266c8c94e530a48efe758
Alexander Matkarimov <56232920+DagothMor@users.noreply.github.com>
2023-09-27 15:55:56 +0300

    Lesson6

---
+++ Lesson6.fs
@@ -0,0 +1,22 @@
+﻿// 17.1
+let rec pow(s,n) = 
+    match s,n with
+    | s,n when n < 1 -> ""
+    | s,n -> string s + pow(s,(n-1))
+
+// 17.2
+let rec isIthChar(s,n,c) = 
+        match (n) with
+        | n when n < 0 -> false
+        | n when n > String.length(s) -> false
+        | n when n = 0 -> s.[0] = c
+        | n -> isIthChar((s.Substring(1)),(n-1),c)
+
+// 17.3
+let rec occFromIth(s,n,c) = 
+    match (n) with
+    | n when n < 0 -> 0
+    | n when n >= String.length(s) -> 0
+    | n -> if s.[n] = c 
+            then 1 + occFromIth(s,(n+1),c) 
+            else 0 + occFromIth(s,(n+1),c)
--- Program.fs
+++ Program.fs
@@ -32,10 +32,37 @@ let main argv =
             false
         else
             prime(num, int maxDiv)
-       
+    
+    let circleLen = fun R -> 2.0 * 3.14 * R
         
 
+    // 17.1 Напишите функцию pow: string * int -> string, где pow(s,n) выдаёт строку s, повторенную n раз.
+    let rec pow(s,n) = 
+        match s,n with
+        | s,n when n < 1 -> ""
+        | s,n -> string s + pow(s,(n-1))
+    
+    // 17.2 Напишите функцию-предикат isIthChar: string * int * char -> bool, где isIthChar(s,n,c) проверяет, равен ли n-й (начиная с нуля) символ строки s символу c.
+    let rec isIthChar(s,n,c) = 
+            match (n) with
+            | n when n < 0 -> false
+            | n when n >= String.length(s) -> false
+            | n when n = 0 -> s.[0] = c
+            | n -> isIthChar((s.Substring(1)),(n-1),c)
+    
+    // 17.3 Напишите функцию occFromIth: string * int * char -> int, где occFromIth(s,n,c) 
+    // возвращает количество вхождений символа с в строке s, начиная с позиции n.
+    let rec occFromIth(s,n,c) = 
+        match (n) with
+        | n when n < 0 -> 0
+        | n when n >= String.length(s) -> 0
+        | n -> if s.[n] = c 
+                then 1 + occFromIth(s,(n+1),c) 
+                else 0 + occFromIth(s,(n+1),c)
 
      // Call the function
-    printfn "%b" (prime(808))
+    printfn "%s" (pow("kakashki",2))
     0 // return an integer exit code
+    //
+    //| n when s.[n] = c -> 1 + occFromIth((s.Substring(1)),(n+1),c)
+    //| n -> 0 + occFromIth((s.Substring(1)),(n+1),c)
